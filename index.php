f24f97247765647732bb364d09a1684cf217b753
Stanislas-Scibilia <91742063+Stanislas-Scibilia@users.noreply.github.com>
2021-11-08 14:40:03 +0100

    ajout de la structure de la page d'accueil

--- index.php
+++ index.php
@@ -1,4 +1,59 @@
 <?php require 'elements/header.php' ?>
 <?php require 'elements/navbar.php' ?>
 
+<section>
+    <article>
+        <a href="connexion.php" class="bouton">
+            
+            <!-- image de connexion -->
+            <img src="" alt="">
+
+            <div class="text-bouton">
+                CONNEXION
+            </div>
+
+        </a>
+
+        <form action="" method="post">
+            <div class="presentation-inscription">Contacte ta salle pour récupérer ton identifiant.</div>
+            <input type="text" name="identifiant" id="identifiant" placeholder="identifiant">
+            <input type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
+            <input type="submit" value="S'inscrire">
+        </form>
+    </article>
+
+    <article>
+        <h1>
+            Qui sommes-nous ?
+        </h1>
+
+        <div class="contenu">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quae laboriosam ab ullam eligendi dignissimos aut consequatur. Perspiciatis explicabo, dolorem rem non voluptate similique nemo voluptates ad odit accusantium et.
+        </div>
+</article>
+</section>
+
+<section>
+    <div class="capteur">
+        <img src="" alt="">
+        <div class="text">
+            capteur cardiaque
+        </div>
+    </div>
+
+    <div class="capteur">
+        <img src="" alt="">
+        <div class="text">
+            capteur CO2
+        </div>
+    </div>
+
+    <div class="capteur">
+        <img src="" alt="">
+        <div class="text">
+            capteur sonore
+        </div>
+    </div>
+</section>
+
 <?php require 'elements/footer.php' ?>
