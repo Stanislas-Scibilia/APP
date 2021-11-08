bb4448a3385776527f4728a3f321c39afd0460d4
Stanislas-Scibilia <91742063+Stanislas-Scibilia@users.noreply.github.com>
2021-11-08 15:28:11 +0100

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,16 @@
+<?php require 'elements/header.php' ?>
+<?php require 'elements/navbar.php' ?>
+
+<div class="contenant">
+
+    <div class="perf"></div>
+    <div class="donnes-env"></div>
+    <div class="perf-ami"></div>
+</div>
+
+<aside>
+    <h1>FAQ</h1>
+    <div class="question"></div>
+</aside>
+
+<?php require 'elements/footer.php' ?>
