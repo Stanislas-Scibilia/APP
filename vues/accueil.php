434911bb6e2bb4c642fc5bb6f254c2585d65c168
benevolens1 <benevolens1@pc-clevo.home>
2021-11-12 15:59:09 +0100

    bonne indentation d'accueil.php

--- vues/accueil.php
+++ vues/accueil.php
@@ -3,12 +3,12 @@
         <a href="connexion.php"> Connexion </a>
 
         <form action="" method="post">
-        <div class="element_connexion">Contacte ta salle pour récupérer ton identifiant.</div>
-        <input class="element_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
-	<br>
-        <input class="element_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
-        <br> 
-	<input class="element_connexion" type="submit" value="S'inscrire">
+            <div class="element_connexion">Contacte ta salle pour récupérer ton identifiant.</div>
+            <input class="element_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
+            <br>
+            <input class="element_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
+            <br> 
+            <input class="element_connexion" type="submit" value="S'inscrire">
         </form>
     </article>
 
@@ -19,7 +19,7 @@
         <div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quae laboriosam ab ullam eligendi dignissimos aut consequatur. Perspiciatis explicabo, dolorem rem non voluptate similique nemo voluptates ad odit accusantium et.
         </div>
-</article>
+    </article>
 </section>
 
 <section>
