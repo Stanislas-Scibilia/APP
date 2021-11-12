604a6edcd84a9c31f3d5c134b73bf79a45b8e541
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2021-11-12 16:18:28 +0100

    Update accueil.php

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,25 +1,35 @@
+ <!DOCTYPE html>
+ <html lang="fr">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>fit'analysor</title>
+ </head>
+ <body>
 <section>
     <article class="connexion">
         <a href="connexion.php"> Connexion </a>
 
         <form action="" method="post">
-            <div class="element_connexion">Contacte ta salle pour récupérer ton identifiant.</div>
-            <input class="element_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
-            <br>
-            <input class="element_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
-            <br> 
-            <input class="element_connexion" type="submit" value="S'inscrire">
+        <div class="element_connexion">Contacte ta salle pour <br> récupérer ton identifiant.</div>
+        <input class="element_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
+	<br>
+        <input class="element_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
+        <br> 
+	<input class="element_connexion" type="submit" value="S'inscrire">
         </form>
     </article>
 
     <article class="article1">
-        <div style="font-size : 60px;">
+        <div class="titre_article1">
             <b> Qui sommes-nous ? </b>
         </div>
         <div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quae laboriosam ab ullam eligendi dignissimos aut consequatur. Perspiciatis explicabo, dolorem rem non voluptate similique nemo voluptates ad odit accusantium et.
         </div>
-    </article>
+</article>
 </section>
 
 <section>
