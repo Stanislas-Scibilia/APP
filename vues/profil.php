f3e814d882a559f673830d302154a4fcbb168519
benevolens1 <benevolens1@pc-clevo.home>
2021-11-12 14:44:05 +0100

    ajout de vues

---
+++ vues/profil.php
@@ -0,0 +1,11 @@
+<div class="contenant">
+
+    <div class="perf"></div>
+    <div class="donnes-env"></div>
+    <div class="perf-ami"></div>
+</div>
+
+<aside>
+    <h1>FAQ</h1>
+    <div class="question"></div>
+</aside>
