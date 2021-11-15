fa15d35e1fa4ff2216a69a4ef96fe3962fc74ae9
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2021-11-15 15:27:33 +0100

    Create navbar1.php

---
+++ vues/navbar1.php
@@ -0,0 +1,8 @@
+<nav>
+			<ul class="nav-list">
+				<li class="nav-item"><a href="...">GESTION DES UTILISATEURS</a></li>
+				<li class="nav-item"><a href="...">FAQ</a></li>
+				<li class="nav-item"><a href="...">GENERATION DE L'IDENTIFIANT</a></li>
+				<li class="nav-item"><a href="...">DECONNEXION</a></li>
+			</ul>
+		</nav>
