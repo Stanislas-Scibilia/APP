d50bf0726da6a45c362871514017afde67128bec
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2021-11-15 15:01:52 +0100

    Update navbar.php

--- vues/navbar.php
+++ vues/navbar.php
@@ -1,1 +1,9 @@
-
+<nav>
+			<ul class="nav-list">
+				<li class="nav-item"><a href="...">CONCEPT</a></li>
+				<li class="nav-item"><a href="...">ABONNEMENTS</a></li>
+				<li class="nav-item"><a href="...">FAQ</a></li>
+				<li class="nav-item"><a href="...">CONTACTEZ-NOUS</a></li>
+				<li class="nav-item"><a href="..."><img src="avatar.png" alt="Avatar" /></a></li>
+			</ul>
+		</nav>
