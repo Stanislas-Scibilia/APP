1974ee7ac700a25a2de3e1a2cefa80edd598394d
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2021-11-16 18:09:40 +0100

    modif

--- vues/navbar.php
+++ vues/navbar.php
@@ -2,10 +2,10 @@
 <nav>
 			<ul class="nav-list">
 				<li class="nav-item"><a href="..."><img src="vues/numeric'air.png" alt="logo" /></a></li>
-				<li class="nav-item"><a href="...">CONCEPT</a></li>
+				<li class="nav-image"><a href="...">CONCEPT</a></li>
 				<li class="nav-item"><a href="...">ABONNEMENTS</a></li>
 				<li class="nav-item"><a href="...">FAQ</a></li>
 				<li class="nav-item"><a href="...">CONTACTEZ-NOUS</a></li>
-				<li class="nav-item"><a href="..."><img src="vues/avatar.png" alt="Avatar" /></a></li>
+				<li class="nav-image"><a href="..."><img src="vues/avatar.png" alt="Avatar" /></a></li>
 			</ul>
 		</nav>
