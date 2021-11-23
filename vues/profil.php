e68b10e3b95eb87fd80b14c38a779425093e9af4
benevolens1 <benevolens1@pc-clevo.home>
2021-11-23 17:16:34 +0100

    page profil

--- vues/profil.php
+++ vues/profil.php
@@ -1,11 +1,10 @@
-<div class="contenant">
-
-    <div class="perf"></div>
-    <div class="donnes-env"></div>
-    <div class="perf-ami"></div>
-</div>
+<div class="perf"> <b>performances</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iure illum magni, fugiat, rem id maxime fuga, voluptatum voluptatem provident et! Deleniti eos voluptas magnam illo atque maxime, ad ut?</div>
+<div class="donnees-env"><b>données environementales</b> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, officiis. Assumenda harum repellendus inventore eveniet quas similique incidunt cumque reiciendis totam maiores voluptate aliquam, ipsam facilis tenetur voluptatibus in quaerat.</div>
+<div class="perf-ami"><b>performances des amis</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quidem porro velit quam blanditiis? Sequi sit, consequatur commodi repudiandae hic optio suscipit quod quisquam ipsa voluptatibus repellat tenetur delectus beatae?</div>
 
 <aside>
     <h1>FAQ</h1>
-    <div class="question"></div>
+    <div class="question">
+    <b>question</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rerum ducimus minima quibusdam dolor error amet architecto ea sapiente sit quasi enim dignissimos unde, assumenda dolorum modi. Aliquam, iure? Eius!
+    </div>
 </aside>
