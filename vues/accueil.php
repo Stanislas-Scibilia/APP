692a60e947b61327e3ada3b56bc446ef61b3b684
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2021-11-29 15:17:55 +0100

    Update accueil.php

--- vues/accueil.php
+++ vues/accueil.php
@@ -24,13 +24,13 @@
 
 <section>
         <div class="capteur">
-                <img src="vues/capteur_cardiaque.png" alt="capteur cardiaque" width="100px" height="100px">
+                <img src="vues/capteur_cardiaque.png" alt="capteur cardiaque" width="300px" height="300px">
                 <h2> Capteur cardiaque </h2>
                 
-                <img src="vues/capteur_CO2.png" alt="capteur CO2" width="100px" height="100px">
+                <img src="vues/capteur_CO2.png" alt="capteur CO2" width="300px" height="300px">
                 <h2> Capteur CO2 </h2>
 
-                <img src="vues/capteur_sonore.png" alt="capteur sonore" width="100px" height="100px">
+                <img src="vues/capteur_sonore.png" alt="capteur sonore" width="300px" height="300px">
                 <h2> Capteur sonore </h2>
         </div>
 </section>
