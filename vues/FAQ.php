736f8bb108c6fdc6169da575a12fe79427dc098a
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2021-12-13 14:00:29 +0100

    Create FAQ.php

---
+++ vues/FAQ.php
@@ -0,0 +1,27 @@
+<p class="image-FAQ"><img src="FAQ.png"></p>
+
+		<header>
+			<h1 class="titre-FAQ">FAQ</h1>
+		</header>
+
+		<section>
+			<article>
+				<button id="Question1">> Que propsons-nous ?</button><br />
+				<button id="Question2">> Dans quelle salle pouvons-nous retouver?</button><br />
+				<button id="Question3">> Comment récuperer son identifiant et son mot de passe ?</button><br />
+				<button id="Question4">> Comment utiliser le produit ?</button><br />
+				<div id="Q1">
+					<p>Fit’analysor est un service accessible à tous qui révolutionnera chacune de vos séances sportives, avec un suivi de votre activité en direct, disponible depuis la page profil. Fitanalysor est la technologie d’avenir indispensable pour tout adepte de sport et de bien-être, et met en place statistiques et graphiques pour vous aider au mieux à gérer votre évolution. </p>
+				</div>
+				<div id="Q2">
+					<p>Dans la salle .........</p>
+				</div>
+				<div id="Q3"> 
+					<p>Récupérez votre identifiant et votre mot de passe au près du personnel de la salle .......... </p>
+				</div>
+				<div id="Q4">
+					<p>Il faut lancer Fit’analysor au début de votre séance, ............</p>
+				</div>
+			</article>
+		</section>
+		<script src="FAQ.js"></script>
