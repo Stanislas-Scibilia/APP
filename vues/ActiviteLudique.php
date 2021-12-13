9dfd0bf26c8ae9d9f77b9d7135b7d85a34a91123
ClementArnqud <clas62113@eleve.isep.fr>
2021-12-13 14:07:38 +0100

    Create ActiviteLudique.php

---
+++ vues/ActiviteLudique.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Activite</title>
+    <link rel="stylesheet" href="al2.css">
+  </head>
+  <body>
+    <header>
+        <h1>
+            Activite Ludique
+        </h1>
+    </header>
+    <section>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h2>
+                Bienvenue sur l'activite ludique de
+            Fit'Analysor
+        </h2>
+          <h3>Tentez votre chance en jouant au jeu ludique. Le principe est simple, Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta omnis distinctio laboriosam assumenda, reprehenderit cumque debitis iusto laudantium fugit deserunt alias quasi dolor, a, nesciunt voluptates inventore dolores. Inventore quasi, non totam recusandae sit illo ex odio! Dolore, quasi quo?  
+          </h3>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div class="type1">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus incidunt, quis quas quidem sapiente unde voluptate quaerat odio totam eveniet esse, pariatur est vero rem aliquid! Molestias, expedita facere.  
+          </p>
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div class="type2">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus incidunt, quis quas quidem sapiente unde voluptate quaerat odio totam eveniet esse, pariatur est vero rem aliquid! Molestias, expedita facere.  
+          </p>
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div class="type1">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus incidunt, quis quas quidem sapiente unde voluptate quaerat odio totam eveniet esse, pariatur est vero rem aliquid! Molestias, expedita
+          </p>
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <div class="type2">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus incidunt, quis quas quidem sapiente unde voluptate quaerat odio totam eveniet esse, pariatur est vero rem aliquid! Molestias, expedita facere.  
+          </p>
+          </div>
+          <div class="vid1">
+            <video id="vid" src="vid01.mp4" controls controlsList="nodownload nofullscreen noremoteplayback" loop muted height="300"></video>  
+          </div>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+    </section>
+  </body>
+</html>
