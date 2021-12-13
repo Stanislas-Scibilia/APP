48a03f3a598c50ae0bf9d7105fb52a23ff907483
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2021-12-13 14:19:59 +0100

    Update ActiviteLudique.php

--- vues/ActiviteLudique.php
+++ vues/ActiviteLudique.php
@@ -5,9 +5,6 @@
     </header>
     <section>
         <br>
-        <br>
-        <br>
-        <br>
         <h2>
                 Bienvenue sur l'activite ludique de
             Fit'Analysor
@@ -15,36 +12,11 @@
           <h3>Tentez votre chance en jouant au jeu ludique. Le principe est simple, Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta omnis distinctio laboriosam assumenda, reprehenderit cumque debitis iusto laudantium fugit deserunt alias quasi dolor, a, nesciunt voluptates inventore dolores. Inventore quasi, non totam recusandae sit illo ex odio! Dolore, quasi quo?  
           </h3>
           <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
           <div class="type1">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus incidunt, quis quas quidem sapiente unde voluptate quaerat odio totam eveniet esse, pariatur est vero rem aliquid! Molestias, expedita facere.  
           </p>
           </div>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
           <br>
           <div class="type2">
           <p>
@@ -52,39 +24,11 @@
           </p>
           </div>
           <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
           <div class="type1">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus incidunt, quis quas quidem sapiente unde voluptate quaerat odio totam eveniet esse, pariatur est vero rem aliquid! Molestias, expedita
           </p>
           </div>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
           <br>
           <div class="type2">
           <p>
@@ -94,10 +38,5 @@
           <div class="vid1">
             <video id="vid" src="vid01.mp4" controls controlsList="nodownload nofullscreen noremoteplayback" loop muted height="300"></video>  
           </div>
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
           <br>
     </section>
