74319480eae7071804172ad28533df948f7f070f
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2021-12-13 15:20:05 +0100

    Update accueil.php

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,17 +1,4 @@
 <section>
-    <article class="connexion">
-        <a href="connexion.php"> Connexion </a>
-
-        <form action="" method="post">
-        <div class="element_connexion">Contacte ta salle pour <br> récupérer ton identifiant.</div>
-        <input class="element_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
-	    <br>
-        <input class="element_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
-        <br> 
-	    <input class="element_connexion" type="submit" value="S'inscrire">
-        </form>
-    </article>
-
     <article class="article1">
         <div class="titre_article1">
             <b> Qui sommes-nous ? </b>
