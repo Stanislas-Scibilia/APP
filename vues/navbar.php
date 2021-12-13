35be9ad66cb42222baea4fff70285917196f085c
benevolens1 <benevolens1@pc-clevo.home>
2021-12-13 15:22:47 +0100

    ajout des liens dans la navbar

--- vues/navbar.php
+++ vues/navbar.php
@@ -1,8 +1,8 @@
 <nav>
 	<ul class="nav-list">
-		<li class="nav-item"><a href="..."><img src="vues/fit'analysor.png" alt="logo" width="100px"></a></li>
+		<li class="nav-item"><a href="/"><img src="vues/fit'analysor.png" alt="logo" width="100px"></a></li>
 		<li class="nav-item"><a href="/?fonction=connexion">Connexion</a></li>
-		<li class="nav-item"><a href="...">FAQ</a></li>
-		<li class="nav-item"><a href="...">Activité ludique</a></li>
+		<li class="nav-item"><a href="/?fonction=faq">FAQ</a></li>
+		<li class="nav-item"><a href="/?fonction=activité_ludique.php">Activité ludique</a></li>
 	</ul>
 </nav>
