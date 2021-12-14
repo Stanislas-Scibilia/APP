81e2341f4c459d78ae7c0fbb3a29886104ff2b9a
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2021-12-14 17:00:15 +0100

    Update activité_ludique.php

--- vues/activité_ludique.php
+++ vues/activité_ludique.php
@@ -1,6 +1,6 @@
 <section>
     <div class="imageal1">
-        <img id="imageduhaut" src="imagehaut.jpg"></img>
+        <img id="imageduhaut" src="vues/imagehaut.jpg"></img>
     </div>
     <br>
         <h1 id="aludique">Activite Ludique</h1>
