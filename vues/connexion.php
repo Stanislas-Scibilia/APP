2108238fcafe7fd3b8653789d1133b856509f8df
benevolens1 <benevolens1@pc-clevo.home>
2021-12-14 16:59:25 +0100

    ajout de la page inscription dans la navbar

--- vues/connexion.php
+++ vues/connexion.php
@@ -1,14 +1,14 @@
 <div class="connexion">
-        <div class="bouton_connexion"> <a href="connexion.php"> Connexion </a> </div>
-        <br>
+        
         <div class="element_connexion">Contacte ta salle pour récupérer ton identifiant</div>
 
         <form action="" method="post">
-        <input class="barre_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
-	<br>
-        <input class="barre_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
-        <br> 
+                <input class="barre_connexion" type="text" name="identifiant" id="identifiant" placeholder="identifiant">
+	        <br>
+                <input class="barre_connexion" type="password" name="motdepasse" id="motdepasse" placeholder="mot de passe">
+                <br> 
         </form>
         <br>
-        <div class="bouton_inscription"> <a href="/?fonction=inscription"> Inscription </a> </div>
+        <div class="bouton_connexion"> <a href="connexion.php"> Connexion </a> </div>
+        
 </div>
