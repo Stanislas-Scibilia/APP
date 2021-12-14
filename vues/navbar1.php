939415378b3a401a31c63eed524b56ff10fee4d3
benevolens1 <benevolens1@pc-clevo.home>
2021-12-14 18:26:07 +0100

    avancement de la connexion

--- vues/navbar1.php
+++ vues/navbar1.php
@@ -1,5 +1,6 @@
 <nav>
 	<ul class="nav-list">
+		<li class="nav-item"><a href="/?fonction=accueil"><img src="vues/fit'analysor.png" alt="logo" width="100px"></a></li>
 		<li class="nav-item"><a href="...">GESTION DES UTILISATEURS</a></li>
 		<li class="nav-item"><a href="...">FAQ</a></li>
 		<li class="nav-item"><a href="...">GENERATION DE L'IDENTIFIANT</a></li>
