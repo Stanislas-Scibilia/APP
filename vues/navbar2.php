cbb9fb2c88c7f9f9666f9d59e2c09e7e7c68b321
benevolens1 <benevolens1@pc-clevo.home>
2021-12-14 18:33:04 +0100

    ajout de 'mon compte' dans la navbar

--- vues/navbar2.php
+++ vues/navbar2.php
@@ -5,6 +5,7 @@
 		<li class="nav-item"><a href="/?fonction=accueil"> Données environementales </a></li>
 		<li class="nav-item"><a href="/?fonction=accueil">Performances amis</a></li>
 		<li class="nav-item"><a href="/?fonction=faq">FAQ</a></li>
+        <li class="nav-item"><a href="/?fonction=accueil">Mon compte</a></li>
         <li class="nav-item"><a href="/?fonction=deconnexion">Déconnexion</a></li>
 	</ul>
 </nav>
