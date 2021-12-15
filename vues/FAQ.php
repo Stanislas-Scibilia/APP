2e448a40a11074bffcee57da0aec7336cf54bcd1
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2021-12-15 11:11:16 +0100

    Update FAQ.php

--- vues/FAQ.php
+++ vues/FAQ.php
@@ -6,8 +6,8 @@
 
 		<section>
 			<article>
-				<button id="Question1">> Que propsons-nous ?</button><br />
-				<button id="Question2">> Dans quelle salle pouvons-nous retouver ?</button><br />
+				<button id="Question1">> Que proposons-nous ?</button><br />
+				<button id="Question2">> Dans quelle salle pouvons-nous retrouver ?</button><br />
 				<button id="Question3">> Comment récuperer son identifiant et son mot de passe ?</button><br />
 				<button id="Question4">> Comment utiliser le produit ?</button><br />
 				<div id="Q1">
@@ -17,7 +17,7 @@
 					<p>Dans la salle .........</p>
 				</div>
 				<div id="Q3"> 
-					<p>Récupérez votre identifiant et votre mot de passe au près du personnel de la salle .......... </p>
+					<p>Récupérez votre identifiant et votre mot de passe auprès du personnel de la salle .......... </p>
 				</div>
 				<div id="Q4">
 					<p>Il faut lancer Fit’analysor au début de votre séance, ............</p>
