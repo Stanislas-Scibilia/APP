d98c471ef114eb0962bd4961b508e4b32cbe031b
benevolens1 <benevolens1@pc-clevo.home>
2022-01-07 15:28:24 +0100

    modification profil

--- vues/profil.php
+++ vues/profil.php
@@ -3,14 +3,15 @@
 Bienvenue Stanislas Scibilia !
 
     <div class="perf">
-        performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis?
+        <p>performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis? </p>
+        <img src="vues/graphique-exemple.jpg" alt="">
     </div>
     
     <div class="donnes-env">
-        données environnementales Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, beatae? Sed, aperiam? Nobis iste, non veniam molestias quisquam dicta ea labore odio. Ipsum nemo quidem expedita, velit repudiandae reprehenderit iusto!
+        <p>données environnementales Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, beatae? Sed, aperiam? Nobis iste, non veniam molestias quisquam dicta ea labore odio. Ipsum nemo quidem expedita, velit repudiandae reprehenderit iusto!</p>
     </div>
 
     <div class="perf-ami">
-        performance des amis Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat ex distinctio porro odit ea quas exercitationem, soluta ullam, vero eos recusandae laboriosam excepturi mollitia rem reprehenderit cupiditate non autem.
+       <p>performance des amis Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat ex distinctio porro odit ea quas exercitationem, soluta ullam, vero eos recusandae laboriosam excepturi mollitia rem reprehenderit cupiditate non autem.</p>
     </div>
 </div>
