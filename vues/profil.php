4ddebdd256e97be0d805acf8d3d8be1404b0b3c0
benevolens1 <benevolens1@pc-clevo.home>
2022-01-07 15:15:19 +0100

    redirection vers le profil apres la connection

--- vues/profil.php
+++ vues/profil.php
@@ -1,24 +1,14 @@
 <div class="contenant">
 
     <div class="perf">
-
+        performance
     </div>
     
     <div class="donnes-env">
-        <div class="slider">
-            <div class="slides">
-                <div class="slide"><img src="CO2.jpeg" alt="img CO2"></div>
-                <div class="slide"><img src="capteur_sonore.png" alt=""></div>
-                <div class="slide"><img src="capteur_cardiaque.png" alt=""></div>
-            </div>
-        </div>    
+        données environnementales
     </div>
 
     <div class="perf-ami">
-
+        performance des amis
     </div>
 </div>
-
-<aside>
-    
-</aside>
