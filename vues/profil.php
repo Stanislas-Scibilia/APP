378faf1213223d509d04860cabe246d9305c1d10
lucief24 <92854756+lucief24@users.noreply.github.com>
2022-01-07 15:57:21 +0100

    Update profil.php

--- vues/profil.php
+++ vues/profil.php
@@ -1,14 +1,22 @@
 <div class="contenant">
 
+Bienvenue Stanislas Scibilia !
+
     <div class="perf">
-        performance
+        <h1>Vos performances</h1>
+        <p>performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis? </p>
+        <img src="vues/graphique-exemple.jpg" alt="">
     </div>
     
     <div class="donnes-env">
-        données environnementales
+        <h1>Données environnementales</h1>
+        <p>données environnementales Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, beatae? Sed, aperiam? Nobis iste, non veniam molestias quisquam dicta ea labore odio. Ipsum nemo quidem expedita, velit repudiandae reprehenderit iusto!</p>
+        <img src="vues/graphique-exemple.jpg" alt="">
     </div>
 
     <div class="perf-ami">
-        performance des amis
+        <h1>Performance de vos amis</h1>
+        <p>performance des amis Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat ex distinctio porro odit ea quas exercitationem, soluta ullam, vero eos recusandae laboriosam excepturi mollitia rem reprehenderit cupiditate non autem.</p>
+       <img src="vues/graphique-exemple.jpg" alt="">
     </div>
 </div>
