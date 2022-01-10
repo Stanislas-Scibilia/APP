08d83aba4fcccd82175f31d9ecd21e45aa1ba1e0
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2022-01-10 15:39:52 +0100

    Update gestionutilisateur.js

--- vues/gestionutilisateur.js
+++ vues/gestionutilisateur.js
@@ -4,6 +4,20 @@ function chercher_utilisateur() {
 	let x = document.getElementByClassName('utilisateur');
 	
 	for (i = 0; i < x.length; i++){
-		if (!x[i].innerHTML.toLowerCase().includes(input)) 
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+
+		else {
+			x[i].style.display="liste-utilisateur"
+
+		}
+
 	}
+
+}
+		
+
+
+
 
