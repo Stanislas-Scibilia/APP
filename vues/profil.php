fcbe91bbb4049a9add27f3b7011775440a2f05f1
ClementArnqud <clas62113@eleve.isep.fr>
2022-01-10 15:24:51 +0100

    Update profil.php

--- vues/profil.php
+++ vues/profil.php
@@ -1,193 +1,200 @@
 <div class="contenant">
 
 <h1 id="bienvenueprofil">Bienvenue Stanislas Scibilia !</h1>
-
     <div class="perf">
-        <h1 id="perfh1">Vos performances</h1>
-        <img src="vues/graphique-exemple.jpg" height=200 alt="">
+      <div class="profilefh1">
+        <p>Vos performances</p></div>
+        <div class="flexboxprofile">
         <table style="width:40%">
-  <tr>
-    <th>a</th>
-    <th>b</th>
-    <th>c</th>
-    <th>d</th>
-    <th>e</th>
-    <th>f</th>
-    <th>g</th>
-    <th>h</th>
-    <th>i</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-    <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-</table>
+          <tr id="tab1perf">
+            <th>a</th>
+            <th>b</th>
+            <th>c</th>
+            <th>d</th>
+            <th>e</th>
+            <th>f</th>
+            <th>g</th>
+            <th>h</th>
+            <th>i</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr id="tab2perf">
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+            <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr id="tab1perf">
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          </table>
+          <img src="vues/graphique-exemple.jpg" height=200 alt=""> 
+        </div>
         <p>performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis? </p>
     </div>
-    
+    <br>
     <div class="donnees-env">
-        <h1 id="donnees-envh1">Données environnementales</h1>
-        <img src="vues/graphique-exemple.jpg" height=200 alt="">
+      <div class="profilefh1">
+        <p>Vos performances</p></div>
+        <div class="flexboxprofile">
         <table style="width:40%">
-  <tr>
-    <th>a</th>
-    <th>b</th>
-    <th>c</th>
-    <th>d</th>
-    <th>e</th>
-    <th>f</th>
-    <th>g</th>
-    <th>h</th>
-    <th>i</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-    <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-</table>
-        <p>données environnementales Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, beatae? Sed, aperiam? Nobis iste, non veniam molestias quisquam dicta ea labore odio. Ipsum nemo quidem expedita, velit repudiandae reprehenderit iusto!</p>
+          <tr>
+            <th>a</th>
+            <th>b</th>
+            <th>c</th>
+            <th>d</th>
+            <th>e</th>
+            <th>f</th>
+            <th>g</th>
+            <th>h</th>
+            <th>i</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+            <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          </table>
+          <img src="vues/graphique-exemple.jpg" height=200 alt=""> 
+        </div>
+        <p>performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis? </p>
     </div>
 
-    <div class="perf-ami">
-        <h1 id="perf-amih1">Performances de vos amis</h1>
-        <img src="vues/graphique-exemple.jpg" height=200 alt="">
+      <div class="perf-ami">
+      <div class="profilefh1">
+        <p>Vos performances</p></div>
+        <div class="flexboxprofile">
         <table style="width:40%">
-  <tr>
-    <th>a</th>
-    <th>b</th>
-    <th>c</th>
-    <th>d</th>
-    <th>e</th>
-    <th>f</th>
-    <th>g</th>
-    <th>h</th>
-    <th>i</th>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-    <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-    <td>1</td>
-  </tr>
-</table>
-        <p>performance des amis Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat ex distinctio porro odit ea quas exercitationem, soluta ullam, vero eos recusandae laboriosam excepturi mollitia rem reprehenderit cupiditate non autem.</p>
+          <tr>
+            <th>a</th>
+            <th>b</th>
+            <th>c</th>
+            <th>d</th>
+            <th>e</th>
+            <th>f</th>
+            <th>g</th>
+            <th>h</th>
+            <th>i</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+            <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          </table>
+          <img src="vues/graphique-exemple.jpg" height=200 alt=""> 
+        </div>
+        <p>performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis? </p>
     </div>
-</div>
