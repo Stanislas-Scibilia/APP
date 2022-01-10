5430ac00c205d6dd98a15e80e9f348eceda5bad8
benevolens1 <benevolens1@pc-clevo.home>
2022-01-10 15:16:48 +0100

    suppression du message

--- controleurs/fonctions.php
+++ controleurs/fonctions.php
@@ -3,7 +3,6 @@
 function verification_session($role) {
     if (empty($_SESSION['connexion']) or $_SESSION['connexion'] != $role) {
         $url = '/?fonction=connexion';
-        $message = 'Vous devez vous connecter pour accéder à la ressource précédemment demandée.';
         header('Location: ' . $url);
     }
 }
