73e121abd266867f0ea95ee559ec44ba3e7762a4
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2022-01-10 15:39:55 +0100

    Update gestionutilisateur.php

--- vues/gestionutilisateur.php
+++ vues/gestionutilisateur.php
@@ -2,7 +2,7 @@
 
 	<input id="barrerecherche" onkeyup="chercher_utilisateur()" type="text" name="recherche" placeholder="Rechercher utilisateurs.."> 
 
-	<ol id="listeutilisateur">
+	<ol id="liste-utilisateur">
 		<li class ="utilisateur1"> Utilisateur </li>
 		<li class ="utilisateur2"> Utilisateur </li>
 		<li class ="utilisateur3"> Utilisateur </li>
