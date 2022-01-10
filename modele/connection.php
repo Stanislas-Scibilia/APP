6c47c610a057bf8da578f4b272bae51977c6976b
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2022-01-10 14:08:50 +0100

    Update connection.php

--- modele/connection.php
+++ modele/connection.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $username = "root";
-    $password = "root";
+    $password = "";
     $dbname = "fit_analysor";
     
     // Create connection
@@ -10,4 +10,17 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
+
+    $sql = "SELECT Nom, Prénom FROM utilisateurs";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+
+          while($row = $result->fetch_assoc()) {
+            echo "Nom : " . $row["Nom"]. " ; Prénom : " . $row["Prénom"]. "<br>";
+          }
+        } else {
+          echo "0 results";
+        }
+        $conn->close();
 ?> 
