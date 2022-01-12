8fc4beb8561000a3e6cea13e0c085181db7a5186
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2022-01-12 12:48:29 +0100

    Update fonctions.php

--- controleurs/fonctions.php
+++ controleurs/fonctions.php
@@ -6,9 +6,3 @@ function verification_session($role) {
         header('Location: ' . $url);
     }
 }
-
-function updateUtilisateurs($db, $id_Utilisateur, $Nom, $Prénom, $Adresse_email, $Mot_de_passe, $Type, $Genre, $Date_de_naissance, $Adresse, $Ville, $Code_postal){
-	$reponse = $db->prepare("update utilisateurs set Nom = ?, Prénom = ?, Adresse email = ?, Mot de passe = ?, Type = ?, Genre = ?, Date de naissance = ?, Adresse = ?, Ville = ?, Code postal = ? where id = ?");
-	$reponse->execute(array($_GET["Nom"],$_GET["Prénom"], $_GET["Adresse_mail"],$_GET["Mot_de_passe"], $_GET["Type"], $_GET["Genre"], $_GET["Date_de_naissance"], $_GET["Adresse"], $_GET["Ville"], $_GET["Code_postal"] ));
-	return $reponse;
-}
