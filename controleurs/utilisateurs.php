dd7b1fe8a37fe5b54f09af5e30dd26c4523adf97
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2022-01-14 16:57:34 +0100

    Update utilisateurs.php

--- controleurs/utilisateurs.php
+++ controleurs/utilisateurs.php
@@ -53,13 +53,16 @@ switch($function) {
     
     case 'inscription':
         include("modele/connexionBDD.php");
-        $sql = "UPDATE utilisateurs SET Nom = '$_POST[nom]', Prenom = '$_POST[prenom]', Adresse_email = '$_POST[email]', Mot_de_passe = '$_POST[motdepasse]' , Type = 'Client', Genre = '$_POST[genre]', Date_de_naissance = '$_POST[datenaissance]', Adresse = '$_POST[adresse]', Ville = '$_POST[ville]', Code_postal = '$_POST[codepostal]' WHERE id_Utilisateur = '$_POST[identifiant]' ";
-        if ($conn->query($sql) === TRUE) {
-        echo "Record updated successfully";
+        $erreur="";
+        if ($_POST["motdepasse"] == $_POST["motdepasse2"]){
+            $sql = "UPDATE utilisateurs SET Nom = '$_POST[nom]', Prenom = '$_POST[prenom]', Adresse_email = '$_POST[email]', Mot_de_passe = '$_POST[motdepasse]' , Type = 'Client', Genre = '$_POST[genre]', Date_de_naissance = '$_POST[datenaissance]', Adresse = '$_POST[adresse]', Ville = '$_POST[ville]', Code_postal = '$_POST[codepostal]' WHERE id_Utilisateur = '$_POST[identifiant]' ";
+                if ($conn->query($sql) !== TRUE) {
+                echo "Error updating record: " . $conn->error;
+                }
+            $conn->close();
         } else {
-        echo "Error updating record: " . $conn->error;
+            $erreur="Réessayez";
         }
-        $conn->close();
         $vue = 'inscription';
         break;
     
