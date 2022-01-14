c77ae1687a054046bc0af460c2d6235a9f26eb14
benevolens1 <benevolens1@pc-clevo.home>
2022-01-14 17:35:18 +0100

    mise en forme simple

--- modele/connection.php
+++ modele/connection.php
@@ -1,26 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "";
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "";
 
-<<<<<<< HEAD
-?> 
-=======
-if ($conn->query($sql) === TRUE) {
-  echo "Record updated successfully";
-} else {
-  echo "Error updating record: " . $conn->error;
-}
-
-$conn->close();
-*/
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 ?>
->>>>>>> 1c02dc5f4a9d333b7c659f569caa3821e39880c5
