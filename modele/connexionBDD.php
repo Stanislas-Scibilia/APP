cd17e3e15f7de4190c32de1016ab880523f62236
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2022-01-14 16:57:37 +0100

    Update connexionBDD.php

--- modele/connexionBDD.php
+++ modele/connexionBDD.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "Fit_analysor";
+    $dbname = "fit_analysor";
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
