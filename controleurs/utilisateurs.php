e288a1ce47d6841a734fa2a3250da326b0f7109e
lucief24 <92854756+lucief24@users.noreply.github.com>
2022-01-17 14:37:45 +0100

    tableau

--- controleurs/utilisateurs.php
+++ controleurs/utilisateurs.php
@@ -91,36 +91,7 @@ switch($function) {
 		$sqlget = "SELECT * FROM utilisateurs";
 		$sqldata = $conn->query($sqlget);
 		
-		echo "<table>";
-		echo "<tr><th>identifiant</th><th>Prénom</th><th>Nom</th><th>Adresse e-mail</th><th>Mot de passe</th><th>Type</th><th>Genre</th><th>Date de naissance</th><th>Adresse</th><th>Ville</th><th>Code postal</th>";
 		
-		while($row = $sqldata->fetch_assoc()) {
-			echo "<tr><td>";
-			echo $row['id_Utilisateur'];
-			echo "</td><td>";
-			echo $row['Prenom'];
-			echo "</td><td>";
-			echo $row['Nom'];
-            echo "</td><td>";
-			echo $row['Adresse_email'];
-            echo "</td><td>";
-			echo $row['Mot_de_passe'];
-            echo "</td><td>";
-			echo $row['Type'];
-            echo "</td><td>";
-			echo $row['Genre'];
-            echo "</td><td>";
-			echo $row['Date_de_naissance'];
-            echo "</td><td>";
-			echo $row['Adresse'];
-            echo "</td><td>";
-			echo $row['Ville'];
-            echo "</td><td>";
-			echo $row['Code_postal'];
-			echo "</td></tr>";
-		}
-
-		echo "</table>";
 		$conn->close();
         $vue = 'gestionutilisateur';
         break;
