59684b7f97d4756f59b1aee16e8044a74ae41f95
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2022-01-17 20:50:47 +0100

    Update gestionutilisateur.php

--- vues/gestionutilisateur.php
+++ vues/gestionutilisateur.php
@@ -37,7 +37,7 @@
 		$articles =$sqldata->query('SELECT * FROM articles');
 			if(isset($_GET['q']) AND !empty($_GET['q'])){
 			$q = htmlspecialchars($_GET['q']);
-			$artciles = $sqldata->query('SELECT Prenom FROM articles WHERE Prenom LIKE "%".$q."%" );
+			$artciles = $sqldata->query('SELECT id_Utilisateur FROM articles WHERE id_Utilisateur LIKE "%".$q."%" );
 		}
 
 	<form method="GET">
