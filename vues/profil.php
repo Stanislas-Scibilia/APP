191a307e17b9b1652f59563b2dba27198d048a6d
ClementArnqud <clas62113@eleve.isep.fr>
2022-01-17 15:10:58 +0100

    Update profil.php

--- vues/profil.php
+++ vues/profil.php
@@ -1,26 +1,29 @@
 <!------------?php
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db = 'db';
-    $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+    verification_session('user');
+        $vue = 'profil';
 
-    $data1 = '';
-    $data2 = '';
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '';
+        $db = 'db';
+        $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+        $data1 = '';
+        $data2 = '';
 
 
-    $sql = "SELECT * FROM datasets ";
-    $result = mysqli_query($mysqli, $sql);
+        $sql = "SELECT * FROM datasets ";
+        $result = mysqli_query($mysqli, $sql);
 
 
-    while ($row = mysqli_fetch_array($result)) {
+        while ($row = mysqli_fetch_array($result)) {
 
-        $data1 = $data1 . '"'. $row['data1'].'",';
-        $data2 = $data2 . '"'. $row['data2'] .'",';
-    }
+            $data1 = $data1 . '"'. $row['data1'].'",';
+            $data2 = $data2 . '"'. $row['data2'] .'",';
+        }
 
-    $data1 = trim($data1,",");
-    $data2 = trim($data2,",");
+        $data1 = trim($data1,",");
+        $data2 = trim($data2,",");
 ?>----->
 
 <div class="contenant">
@@ -32,77 +35,36 @@
 <div id="perf">
       <div class="profilefh1">
         <p>Vos performances</p></div>
-        <div class="flexboxprofile">
-          <div class="fondprofile">
-          <div class="tableprofile1">
-          <table>
-          <tr>
-            <th>a</th>
-            <th>b</th>
-            <th>c</th>
-            <th>d</th>
-            <th>e</th>
-            <th>f</th>
-            <th>g</th>
-            <th>h</th>
-            <th>i</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-            <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          </table>
-          </div>
-          </div>
-          <div class="fondprofile">
-          <div class="tableprofile2">
-            <canvas id="graphprophile"></canvas>
+        <div class="bodyprofile">
+      <div class="containerprofile">
+      <h1> le graphique </h1>
+        <canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+        <script>
+          var ctx = document.getElementById("chart").getContext('2d');
+          var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: [1,2,3,4,5,6,7,8,9,10],
+              datasets: 
+              [{
+                  label: 'Données',
+                  data: [<?php echo $data1; ?>],
+                  backgroundColor: 'transparent',
+                  borderColor:'#e91e63',
+                  borderWidth: 3
+              }]
+          },
 
-              <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-              <script src="vues/graphiqueprofile.js"></script>
-          </div>
-          </div> 
-        </div>
+          options: {
+          scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
+          tooltips:{mode: 'index'},
+          legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
+          }
+          });
+          </script>
+      </div>
+      </div>  
         <br>
         <div class="texteprofile">
         <p>performance Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dicta nemo cum vitae harum deleniti quo est, perspiciatis ad sapiente voluptate in vero dolores. Nobis quas itaque facere ullam officiis? </p>
@@ -345,41 +307,4 @@
       <br>
       <br>
       <br>
-      <div class="bodyprofile">
-      <div class="containerprofile">
-      <h1> le graphique </h1>
-        <canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-        <script>
-          var ctx = document.getElementById("chart").getContext('2d');
-          var myChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-              labels: [1,2,3,4,5,6,7,8,9],
-              datasets: 
-              [{
-                  label: 'Data 1',
-                  data: [<?php echo $data1; ?>],
-                  backgroundColor: 'transparent',
-                  borderColor:'#e91e63',
-                  borderWidth: 3
-              },
-
-              {
-                  label: 'Data 2',
-                  data: [<?php echo $data2; ?>],
-                  backgroundColor: 'transparent',
-                  borderColor:'rgba(0,255,255)',
-                  borderWidth: 3
-              }]
-          },
-
-          options: {
-          scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
-          tooltips:{mode: 'index'},
-          legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
-          }
-          });
-          </script>
-      </div>
-      </div>
+      
