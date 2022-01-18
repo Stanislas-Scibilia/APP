d65164a2ba546d0adf549bc3b8b40cbd441bbd40
lucief24 <92854756+lucief24@users.noreply.github.com>
2022-01-18 18:30:54 +0100

    Update gestionutilisateur.js

--- vues/gestionutilisateur.js
+++ vues/gestionutilisateur.js
@@ -1,10 +1,11 @@
-function chercher_utilisateur(str) {
-
-  input = document.getElementById("recherche");
-  filter = input.value;
-  table = document.getElementById("utilisateurs");
-  tr = table.getElementsByTagName("tr");
-
+function chercher_utilisateur() {
+	var input, filter, table, tr, td, i, txtValue;
+  	input = document.getElementById("recherche");
+	  
+ 	filter = input.value.toUpperCase();
+ 	table = document.getElementById("utilisateurs");
+ 	tr = table.getElementsByTagName("tr");
+	 
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[1];
     if (td) {
