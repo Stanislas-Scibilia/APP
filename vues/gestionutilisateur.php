c09cad35e8144a48ed4170395b3b103e1274450e
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2022-01-18 17:35:26 +0100

    Update gestionutilisateur.php

--- vues/gestionutilisateur.php
+++ vues/gestionutilisateur.php
@@ -1,42 +1,4 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=Fit_analysor;', 'root');
-$allusers = $bdd->query('SELECT * FROM utilisateurs ORDER BY id_Utilisateur DESC');
-if(isset($_GET['s']) AND !empty($_GET['s'])){
-	$recherche = htmlspecialchars($_GET['s']);
-	$allusers = $bdd->query('SELECT Nom, Prenom, id_Utilisateur,Genre,Adresse_email, Mot_de_passe FROM utilisateurs WHERE Nom LIKE"%'.$recherche.'%" ORDER BY id DESC');
-}
-?>
-<body>
 
-	<form method="GET">
-		<input type="search" name="s" placeholder="Rechercher un utilisateur" autocomplete="off">
-		<input type="submit" name="envoyer">
-	</form>
-<<<<<<< HEAD
-<<<<<<< HEAD
-	
-	<section class="afficher_utilisateur">
-
-		<?php
-			if($allusers->rowCount() > 0){
-				while($user = $allusers->fetch()){
-					?>
-					<p><?= $user['Nom']; ?> <br/><?= $user['Prenom'] ?> <br/> <?= $user['id_Utilisateur'] ?> <br/><?= $user['Genre'] ?> <br/> <?= $user['Adresse_email'] ?> <br/> <?= $user['Mot_de_passe'] ?></p>
-					<?php  
-				}
-
-			}else{
-				?>
-				<p>Aucun utilisateur trouvé</p>
-				<?php
-			}
-
-		?>
-	</section>
-</body>
-=======
-=======
->>>>>>> 3c4e8ca22875b89e3b2bf5d24d57162f02db9b80
 
 	<input type="text" id="recherche" onkeyup="chercher_utilisateur()" placeholder="Rechercher un utilisateur..."> 
 	<?php
