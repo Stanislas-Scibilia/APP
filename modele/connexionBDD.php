38b60e51a6e98cac71acb4491932b9dfbdf24e8b
camgrd <giordancamille@gmail.com>
2022-01-18 16:07:57 +0100

    Update connexionBDD.php

--- modele/connexionBDD.php
+++ modele/connexionBDD.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $username = "root";
-    $password = "";
+    $password = "root";
     $dbname = "fit_analysor";
     
     $conn = new mysqli($servername, $username, $password, $dbname);
