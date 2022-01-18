a1c280bb0875b12249a96fa78925a5ea03475d3a
Arthurclnt <93603844+Arthurclnt@users.noreply.github.com>
2022-01-18 01:35:05 +0100

    Update gestionutilisateur.php

--- vues/gestionutilisateur.php
+++ vues/gestionutilisateur.php
@@ -3,7 +3,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=Fit_analysor;', 'root');
 $allusers = $bdd->query('SELECT * FROM utilisateurs ORDER BY id_Utilisateur DESC');
 if(isset($_GET['s']) AND !empty($_GET['s'])){
 	$recherche = htmlspecialchars($_GET['s']);
-	$allusers = $bdd->query('SELECT Nom, Prénom, id_Utilisateur,Genre,Adresse_email, Mot_de_passe FROM utilisateurs WHERE Nom LIKE"%'.$recherche.'%" ORDER BY id DESC');
+	$allusers = $bdd->query('SELECT Nom, Prénom, id_Utilisateur,Genre,Adresse email, Mot de passe FROM utilisateurs WHERE Nom LIKE"%'.$recherche.'%" ORDER BY id DESC');
 }
 ?>
 <body>
@@ -19,7 +19,7 @@ if(isset($_GET['s']) AND !empty($_GET['s'])){
 			if($allusers->rowCount() > 0){
 				while($user = $allusers->fetch()){
 					?>
-					<p><?= $user['Nom']; ?> <br/> <?= $user['Prénom'] ?> <br/> <?= $user['id_Utilisateur'] ?> <br/> <?= $user['Genre'] ?> <br/> <?= $user['Adresse_email'] ?> <br/> <?= $user['Mot_de_passe'] ?></p>
+					<p><?= $user['Nom']; ?> <br/><<?= $user['Prénom'] ?> <br/> <?= $user['id_Utilisateur'] ?> <br/><?= $user['Genre'] ?> <br/> <?= $user['Adresse email'] ?> <br/> <?= $user['Mot de passe'] ?></p>
 					<?php  
 				}
 
