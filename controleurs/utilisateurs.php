c3d2b6e2f985915de8737b97a005c3ff3ee55a80
lucief24 <92854756+lucief24@users.noreply.github.com>
2022-01-19 11:43:06 +0100

    Update utilisateurs.php

--- controleurs/utilisateurs.php
+++ controleurs/utilisateurs.php
@@ -179,7 +179,19 @@ switch($function) {
     default:
         $vue = '404';
         break;
+    
+    case 'modifform':
+        verification_session('admin');
+        include ('modele/connexionBDD.php');
+        $vue = 'modifform';
+        $sql = "UPDATE utilisateurs SET Nom = '$_POST[nom]', Prenom = '$_POST[prenom]', Adresse_email = '$_POST[email]', Mot_de_passe = '$_POST[motdepasse]' , Genre = '$_POST[genre]', Date_de_naissance = '$_POST[datenaissance]', Adresse = '$_POST[adresse]', Ville = '$_POST[ville]', Code_postal = '$_POST[codepostal]' WHERE id_Utilisateur = '$_POST[identifiant]' ";
+                if ($conn->query($sql) !== TRUE) {
+                echo "Error updating record: " . $conn->error;
+                }
+            $conn->close();
+    break;
 }
+
 
 include('vues/header.php');
 
