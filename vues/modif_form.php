6aa7750122c2e62f10eeb67dcadf6d75dab8a22a
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2022-01-21 14:53:16 +0100

    form modif

--- vues/modif_form.php
+++ vues/modif_form.php
@@ -1,10 +1,9 @@
 <div class="container">
     <h1>Modification</h1>
-    <?php echo $erreur ?>
+    <?php echo $confirmation ?>
     <form action="" method="post">
-           
         <input class="barre_inscription" type="text" name="identifiant" id="identifiant" placeholder="Identifiant" maxlength="50">
-        <br>
+        <br> 
         <input class="barre_inscription" type="text" name="prenom" id="prenom" placeholder="Prénom" maxlength="50">
         <br>
         <input class="barre_inscription" type="text" name="nom" id="nom" placeholder="Nom" maxlength="50">
@@ -28,15 +27,6 @@
         <input class="barre_inscription" type="text" name="codepostal" id="codepostal" placeholder="Code postal" maxlength="50">
         <br>
         <input class="barre_inscription" type="text" name="ville" id="ville" placeholder="Ville" maxlength="50">
-        <br>
-        <input class="barre_inscription" type="password" name="motdepasse" id="motdepasse" placeholder="Mot de passe" maxlength="50">
-        <br>
-        <input class="barre_inscription" type="password" name="motdepasse2" id="motdepasse2" placeholder="Confirmez votre mot de passe" maxlength="50">
-        <br>
-        <p>Au moins 8 caractères et doit comporter des caractères spéciaux et numériques</p>
-        <br>
-        <input type="checkbox" name="CGU" id="CGU">
-        <label for="CGU">Accepter les conditions générales d'utilisation et les règles de confidentialité.</label>
         <br>
         <input type="submit" value="Modifier les informations">
     </form>
